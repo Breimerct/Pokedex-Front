c84546b31c874a9ac6b79d00f922167c35c5f0d4
Breimerct98 <breimer.correa@zabud.com.co>
2021-12-22 16:02:28 -0500

    the favicon was changed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Pokedex",
+    themeColor: "#dc0a2d",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#dc0a2d",
+  },
 };
